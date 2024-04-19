4204822e72c8ad47f31311d311a40a72432e73a5
Anton <grytcaie@gmail.com>
2024-04-19 14:41:59 +0300

    update active_admin panel

--- app/admin/films.rb
+++ app/admin/films.rb
@@ -1,5 +1,40 @@
 ActiveAdmin.register Film do
-  permit_params :title, :description, :genre_id, :year, :image, :author
+  permit_params :title, :description, :genre_id, :year, :image, :author_id
+
+  index do
+    selectable_column
+    column :id
+    column :title
+    column :description
+    column :genre
+    column :year
+    column "Image" do |film|
+      if film.image.attached?
+        image_tag film.image.variant(resize: '100x100')
+      else
+        "No Image"
+      end
+    end
+    column :author
+    actions
+  end
+
+  show do
+    attributes_table do
+      row :title
+      row :description
+      row :genre
+      row :year
+      row "Image" do |film|
+        if film.image.attached?
+          image_tag film.image.variant(resize: '300x300')
+        else
+          "No Image"
+        end
+      end
+      row :author
+    end
+  end
 
   form do |f|
     f.inputs do
--- app/models/film.rb
+++ app/models/film.rb
@@ -4,6 +4,9 @@ class Film < ApplicationRecord
   has_one_attached :image
   belongs_to :genre
   belongs_to :author
+  validate :image_content_type
+  validate :image_size
+
 
   def self.ransackable_attributes(_auth_object = nil)
     %w[title description year genre_id producer created_at updated_at image_filename]
@@ -13,4 +16,18 @@ class Film < ApplicationRecord
   def self.ransackable_associations(_auth_object = nil)
     %w[comments]
   end
+
+  private
+  def image_content_type
+    if image.attached? && !image.content_type.in?(%w[image/jpeg image/png])
+      errors.add(:image, "must be a JPEG or PNG file")
+    end
+  end
+
+  def image_size
+    if image.attached? && image.blob.byte_size > 2.megabytes
+      errors.add(:photo, "must be less than 2MB")
+    end
+  end
+
 end
