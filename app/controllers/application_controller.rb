9501baf11dd6dcee2b9c07244ac6ff9e5182b4dc
anton <grytcaie@gmail.com>
2024-04-26 10:33:18 +0300

    add link for multi-language

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,19 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  before_action :set_locale
 
   def authenticate_admin!
     redirect_to root_path, alert: 'You are not authorized to access this page.' unless current_user&.admin?
   end
+
+  private
+
+  def set_locale
+    I18n.locale = params[:locale] || I18n.default_locale
+  end
+
+  def default_url_options
+    { locale: I18n.locale }
+  end
+
 end
